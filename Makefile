376fe3e7a2c7957df3f00b94559fe74c696a9fd2
bazsup <bas10600@gmail.com>
2022-05-25 11:15:18 +0700

    add gracefully shutdown

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 create_todo:
 	restcli --show body ./test/create_todos.http
+
+kill:
+	lsof -i :8081 | awk 'NR > 1 {print $$2}' | xargs kill -SIGTERM
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -41,5 +46,30 @@ func main() {
 	handler := todo.NewTodoHandler(db)
 	protected.POST("/todos", handler.NewTask)
 
-	r.Run()
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+	s := &http.Server{
+		Addr:           ":" + os.Getenv("PORT"),
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+	fmt.Println("shutting down gracefully, press ctrl+c again to force")
+
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(timeoutCtx); err != nil {
+		fmt.Println(err)
+	}
 }
